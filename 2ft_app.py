b3f9bce05cadbe2e7a63ac50351d7c22c5f6d8fd
bulloch1 <richardbulloch1@gmail.com>
2026-02-16 21:15:55 -0700

    Update 2ft_app.py

--- 2ft_app.py
+++ 2ft_app.py
@@ -44,12 +44,22 @@ def PageContents():
     return height, foot_length, foot_width, weight, right, advanced_options    
 
 def GetShape():
+
+
+
+    
     def getAdvancedMeasurements():
         predicted_pylon_radius = 30 #TODO make this reflect weight or proportional to given measurement
         predicted_ankle_height = foot_length*0.3 # height where foot meets pylon
         predicted_toe_height = predicted_ankle_height*0.5
         predicted_pylon_offset = predicted_pylon_radius*1.2
         return ankle_height, toe_height, pylon_offset, pylon_radius
+
+
+
+
+
+    
         
     height, foot_length, foot_width, weight, right, advanced_options  = PageContents()
     heel_radius = 0.4 * foot_width
@@ -222,6 +232,7 @@ def ExportSTL(result):
 
 
 ExportSTL(GetShape())
+
 
 
 
