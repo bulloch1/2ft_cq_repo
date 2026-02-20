c2f7c48c41fb7610c1c4b76381a6a933cb24b9cb
bulloch1 <richardbulloch1@gmail.com>
2026-02-19 19:07:42 -0700

    Update 2ft_app.py

--- 2ft_app.py
+++ 2ft_app.py
@@ -20,7 +20,7 @@ def PageContents():
     lb_per_kg = 2.20462
         
     #page elements
-    st.title("2ft Custom Prosthesis 305")
+    st.title("2ft Custom Prosthesis 707")
     metric = st.toggle("Use metric units (mm, kg)", value = True)
     if metric:
         foot_length = st.slider("Foot Length (mm)", foot_length_lb, foot_length_ub, value = foot_length_avg)
@@ -232,6 +232,7 @@ def ExportSTL(result):
 
 
 ExportSTL(GetShape())
+
 
 
 
