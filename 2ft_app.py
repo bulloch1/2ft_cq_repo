25653a1b5e3390a71b3284fc0eab0050e186929b
bulloch1 <richardbulloch1@gmail.com>
2026-02-21 21:28:37 -0700

    Update 2ft_app.py
    comment everything except page cotents for testing

--- 2ft_app.py
+++ 2ft_app.py
@@ -90,180 +90,182 @@ def GetShape():
     heel_radius = 0.4 * foot_width #this value makes the medial edge of the heel vertical (40% of foot is medial, 60% is lateral)
     ankle_height, toe_height, pylon_offset, pylon_radius = getAdvancedMeasurements()
     
-    pylon_height = height - ankle_height
-    toe_length = 0.14 * foot_length
+#     pylon_height = height - ankle_height
+#     toe_length = 0.14 * foot_length
      
-    #pyramid adapter
-    ball_base_radius = 47.8/2
-    ball_depth = 10.7
-    dove_depth = 11.1
-    dove_tail_width = 18.3
-    dove_base_width = 14
+#     #pyramid adapter
+#     ball_base_radius = 47.8/2
+#     ball_depth = 10.7
+#     dove_depth = 11.1
+#     dove_tail_width = 18.3
+#     dove_base_width = 14
     
-    lateral_vector = (heel_radius - 0.6*foot_width, heel_radius - 0.66*foot_length)
-    toe_vector = (1, -0.6)
+#     lateral_vector = (heel_radius - 0.6*foot_width, heel_radius - 0.66*foot_length)
+#     toe_vector = (1, -0.6)
      
-    #defines footprint shape
-    big_toe_pt = (-0.2*heel_radius, foot_length)#
-    little_toe_pt = (0.4*foot_width, foot_length*0.94)
-    ball_y = 0.66*foot_length # y distance of the widest part of the foot, where foot_width is measured
-    footprint_spline_pts = [
-        (0, 0),#base of heel
-        (-heel_radius, heel_radius), #left side
-        (-heel_radius, foot_length - (toe_length)),#connects medial edge to big toe
-        (big_toe_pt),
-        (little_toe_pt),
-        (0.6*foot_width, ball_y), #TODO make right side end exactly at foot_width*0.6
-        (heel_radius, heel_radius), #right side of heel
-        (0.5*heel_radius, 0.2*heel_radius),
-        (0, 0),#back to starting pt
-    ]
+#     #defines footprint shape
+#     big_toe_pt = (-0.2*heel_radius, foot_length)#
+#     little_toe_pt = (0.4*foot_width, foot_length*0.94)
+#     ball_y = 0.66*foot_length # y distance of the widest part of the foot, where foot_width is measured
+#     footprint_spline_pts = [
+#         (0, 0),#base of heel
+#         (-heel_radius, heel_radius), #left side
+#         (-heel_radius, foot_length - (toe_length)),#connects medial edge to big toe
+#         (big_toe_pt),
+#         (little_toe_pt),
+#         (0.6*foot_width, ball_y), #TODO make right side end exactly at foot_width*0.6
+#         (heel_radius, heel_radius), #right side of heel
+#         (0.5*heel_radius, 0.2*heel_radius),
+#         (0, 0),#back to starting pt
+#     ]
     
-    footprint_tangents = [
-        (-1, 0), #back of heel is horizontal
-        (0, 1), #tangent with medial edge
-        (0, 1),
-        (1, 0),#toe must be horizontal at foot_length
-        (toe_vector),
-        (lateral_vector),
-        (lateral_vector), #forces tangecy with right (lateral) edge of foot
-        (None),
-        (-1, 0)
-    ]
+#     footprint_tangents = [
+#         (-1, 0), #back of heel is horizontal
+#         (0, 1), #tangent with medial edge
+#         (0, 1),
+#         (1, 0),#toe must be horizontal at foot_length
+#         (toe_vector),
+#         (lateral_vector),
+#         (lateral_vector), #forces tangecy with right (lateral) edge of foot
+#         (None),
+#         (-1, 0)
+#     ]
     
-    #defines top of foot
-    ankle_x = pylon_radius*2.4
-    ankle_point = (ankle_x, ankle_height) #end of the arch, where the front of the pylon will connect
-    arch_spline_pts = [
-        (foot_length, toe_height), 
-        # (ankle_x+(foot_length-ankle_x)*0.55, toe_height+((ankle_height-toe_height)*0.12)), #((55% between ankle and toe), (12% between toe height and ankle height))
-        (ball_y, toe_height+((ankle_height-toe_height)*0.2)), #((55% between ankle and toe), (12% between toe height and ankle height))
-        (ankle_point) 
-    ]
+#     #defines top of foot
+#     ankle_x = pylon_radius*2.4
+#     ankle_point = (ankle_x, ankle_height) #end of the arch, where the front of the pylon will connect
+#     arch_spline_pts = [
+#         (foot_length, toe_height), 
+#         # (ankle_x+(foot_length-ankle_x)*0.55, toe_height+((ankle_height-toe_height)*0.12)), #((55% between ankle and toe), (12% between toe height and ankle height))
+#         (ball_y, toe_height+((ankle_height-toe_height)*0.2)), #((55% between ankle and toe), (12% between toe height and ankle height))
+#         (ankle_point) 
+#     ]
     
-    arch_tangents = [
-        (-1, 0),
-        (None),
-        (-0.4, 1),
-    ]
+#     arch_tangents = [
+#         (-1, 0),
+#         (None),
+#         (-0.4, 1),
+#     ]
     
-    def getArch():
-        arch = (
-            cq.Workplane("right")
-            .workplane(offset = -0.4*foot_width)
-            .spline(arch_spline_pts, arch_tangents)
-            .lineTo(foot_length*2, ankle_height)
-            .lineTo(foot_length*2, toe_height)
-            .close()
-            .extrude(foot_width*2)
-        )
-        return arch
+#     def getArch():
+#         arch = (
+#             cq.Workplane("right")
+#             .workplane(offset = -0.4*foot_width)
+#             .spline(arch_spline_pts, arch_tangents)
+#             .lineTo(foot_length*2, ankle_height)
+#             .lineTo(foot_length*2, toe_height)
+#             .close()
+#             .extrude(foot_width*2)
+#         )
+#         return arch
     
-    def Foot():
-        foot = (
-            cq.Workplane("top")
-            .spline(footprint_spline_pts, footprint_tangents)
-            .close()
-            .extrude(ankle_height)
-            .cut(getArch())
-            .combine()
-            .clean()
-            .faces("<<Y[1]")
-            .edges("not |X")
-            .chamfer(toe_height*0.4)
-            .faces("<<Y[2]")
-            .edges("not <<Y[1] or <<Y[0]")
-            .chamfer(toe_height*0.2)
-            # .faces("<Y")
-            # .chamfer(toe_height*0.1)
-        )
-        return foot
+#     def Foot():
+#         foot = (
+#             cq.Workplane("top")
+#             .spline(footprint_spline_pts, footprint_tangents)
+#             .close()
+#             .extrude(ankle_height)
+#             .cut(getArch())
+#             .combine()
+#             .clean()
+#             .faces("<<Y[1]")
+#             .edges("not |X")
+#             .chamfer(toe_height*0.4)
+#             .faces("<<Y[2]")
+#             .edges("not <<Y[1] or <<Y[0]")
+#             .chamfer(toe_height*0.2)
+#             # .faces("<Y")
+#             # .chamfer(toe_height*0.1)
+#         )
+#         return foot
     
-    def AddPylon(foot):
-        foot = (
-            foot.faces(">Y")
-            .edges()
-            .toPending()
-            .offset2D(0)
-            .workplane(offset = pylon_height)
-            .center(0, pylon_offset)
-            .ellipse(pylon_radius, pylon_radius*1.2)
-            .loft(combine = True)
-        )
-        return foot
+#     def AddPylon(foot):
+#         foot = (
+#             foot.faces(">Y")
+#             .edges()
+#             .toPending()
+#             .offset2D(0)
+#             .workplane(offset = pylon_height)
+#             .center(0, pylon_offset)
+#             .ellipse(pylon_radius, pylon_radius*1.2)
+#             .loft(combine = True)
+#         )
+#         return foot
     
-    def CutPyramidAdapter(foot):
-        base_height = 8
-        adapter_base = (
-            cq.Workplane("right")
-            .center(pylon_offset, pylon_height+ankle_height - base_height/2)
-            .box(62, base_height, pylon_radius*2)
-            .edges("|Y")
-            .fillet(8)
-        )
-        foot = foot.union(adapter_base).clean()
+#     def CutPyramidAdapter(foot):
+#         base_height = 8
+#         adapter_base = (
+#             cq.Workplane("right")
+#             .center(pylon_offset, pylon_height+ankle_height - base_height/2)
+#             .box(62, base_height, pylon_radius*2)
+#             .edges("|Y")
+#             .fillet(8)
+#         )
+#         foot = foot.union(adapter_base).clean()
         
-        adapter = (
-            cq.Workplane("right")
-            .center(pylon_offset, pylon_height+ankle_height)
-            .lineTo(24, 0) # build ball joint profile
-            .threePointArc((17, -7.5), (11.4, -10.8))
-            .lineTo(0, -10.8)
-            .close()
-            .revolve(90, (0, 0), (0, -1)) #revolve ball joint
-            .center(0, -ball_depth)
-            .lineTo(dove_base_width/2, 0)#dove tail outline
-            .lineTo(dove_tail_width/2, -dove_depth)
-            .lineTo(0, -dove_depth)
-            .close()
-            .extrude(dove_base_width/2)
-            .faces("<X")
-            .extrude(-pylon_radius*2)
-        )
-        adapter = (
-            adapter
-            .mirror(adapter.faces(">Z"), union = True)
-            .faces("<Y")
-            .fillet(2)
-            .faces("<<Y[4]")
-            .edges("|X")
-            .fillet(1)
-        )
-        foot = foot.cut(adapter)
-        return foot
+#         adapter = (
+#             cq.Workplane("right")
+#             .center(pylon_offset, pylon_height+ankle_height)
+#             .lineTo(24, 0) # build ball joint profile
+#             .threePointArc((17, -7.5), (11.4, -10.8))
+#             .lineTo(0, -10.8)
+#             .close()
+#             .revolve(90, (0, 0), (0, -1)) #revolve ball joint
+#             .center(0, -ball_depth)
+#             .lineTo(dove_base_width/2, 0)#dove tail outline
+#             .lineTo(dove_tail_width/2, -dove_depth)
+#             .lineTo(0, -dove_depth)
+#             .close()
+#             .extrude(dove_base_width/2)
+#             .faces("<X")
+#             .extrude(-pylon_radius*2)
+#         )
+#         adapter = (
+#             adapter
+#             .mirror(adapter.faces(">Z"), union = True)
+#             .faces("<Y")
+#             .fillet(2)
+#             .faces("<<Y[4]")
+#             .edges("|X")
+#             .fillet(1)
+#         )
+#         foot = foot.cut(adapter)
+#         return foot
     
-    def AssembleFoot():
-        foot = Foot()
-        foot = AddPylon(foot)
-        foot = CutPyramidAdapter(foot)
+#     def AssembleFoot():
+#         foot = Foot()
+#         foot = AddPylon(foot)
+#         foot = CutPyramidAdapter(foot)
         
-        if (right == False):
-            foot = foot.mirror("YZ")
+#         if (right == False):
+#             foot = foot.mirror("YZ")
         
-        return foot
+#         return foot
     
-    foot = AssembleFoot()
-    return foot
+#     foot = AssembleFoot()
+#     return foot
 
-#end of CadQuery script
+# #end of CadQuery script
 
-def ExportSTL(result):
-    tmp_path = None
-    try:
-        with tempfile.NamedTemporaryFile(suffix=".stl", delete=False) as tmp:
-            tmp_path = tmp.name
-            result.val().export(tmp_path)
+# def ExportSTL(result):
+#     tmp_path = None
+#     try:
+#         with tempfile.NamedTemporaryFile(suffix=".stl", delete=False) as tmp:
+#             tmp_path = tmp.name
+#             result.val().export(tmp_path)
         
-        with open(tmp_path, "rb") as f:
-            stl_bytes = f.read()
+#         with open(tmp_path, "rb") as f:
+#             stl_bytes = f.read()
         
-    except Exception:
-        pass
-    finally:
-        if tmp_path and os.path.exists(tmp_path):
-            os.unlink(tmp_path)
+#     except Exception:
+#         pass
+#     finally:
+#         if tmp_path and os.path.exists(tmp_path):
+#             os.unlink(tmp_path)
 
-    st.download_button("Download STL", stl_bytes, "leg2.stl")
+#     st.download_button("Download STL", stl_bytes, "leg2.stl")
 
 
-ExportSTL(GetShape())
+# ExportSTL(GetShape())
+
+pageContents()
