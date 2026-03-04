cf5bd49bd4411e1d0e120288b6ae746fdec8c1cb
bulloch1 <richardbulloch1@gmail.com>
2026-03-04 08:10:16 -0700

    Update downloadLanding.py

--- pages/downloadLanding.py
+++ pages/downloadLanding.py
@@ -1,7 +1,30 @@
 import streamlit as st
 
+left, right = st.columns(2)
 
 st.title("Download Successful!")
+st.divider()
+st.space("medium")
+left.header("Printing Your Leg - Next Steps:")
+left.space("small")
+left.subheader("Slicer")
+left.markdown("we recommend Cura (link)")
+left.space("small")
+left.subheader("Settings")
+left.markdown("print the foot upright, with __% infill and __ wall thickness. ___ layer height is recommended")
+left.space("small")
+left.header("Wearing Your New Leg)
+left.markdown("Simply slide the leg tightly onto your pyramid adapter")
+
+right.header("Estimated Specs")
+right.space("small")
+right.markdown("weight: __kg")
+right.markdown("material cost: $__ (for PLA at $20/kg)")
+right.markdown("printing time: __hrs (on a ___ printer)")
+right.markdown("estimated lifetime: not very long")
+right.markdown("allowable weight: lots of kilos")
+right.markdown("other cool facts: None")
+
 
 if st.button("Back to Home Page"):
     st.switch_page("2ft_app.py")
